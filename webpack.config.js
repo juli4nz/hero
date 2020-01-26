e0c31e197ea1d50577fad3d7768461bc06062cbc
juli4nz <1939392+juli4nz@users.noreply.github.com>
2020-01-26 14:30:12 +0000

    Fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  rules: [
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	rules: [
+		{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}
+	]
 };
