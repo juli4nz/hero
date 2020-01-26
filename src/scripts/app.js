17e993b72d8048ac089280dd3327e702bdd01e93
juli4nz <1939392+juli4nz@users.noreply.github.com>
2020-01-26 18:50:32 +0000

    Add letters.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import Letters from './els/letters.js';
 
 export default class App {}
--- src/scripts/els/letters.js
+++ src/scripts/els/letters.js
@@ -1,1 +1,71 @@
-export default class Letters {}
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
+
+export default class Letters {
+	setup() {
+		// Get SVG markup from DOM
+		const svgMarkup = document.querySelector('svg#dima').outerHTML;
+
+		// SVG Loader is not a part of the main three.js bundle
+		// we need to load it by hand from:
+		// https://github.com/mrdoob/three.js/blob/master/examples/js/loaders/SVGLoader.js
+		this.loader = new THREE.SVGLoader();
+		this.svgData = loader.parse(svgMarkup);
+
+		// Group we'll use for all SVG paths
+		this.svgGroup = new THREE.Group();
+		// When importing SVGs paths are inverted on Y axis
+		// it happens in the process of mapping from 2d to 3d coordinate system
+		this.svgGroup.scale.y *= -1;
+
+		this.material = new THREE.MeshNormalMaterial();
+	}
+
+	getPaths() {
+		// Loop through all of the parsed paths
+		this.svgData.paths.forEach((path, i) => {
+			const shapes = path.toShapes(true);
+
+			// Each path has array of shapes
+			shapes.forEach((shape, j) => {
+				// Finally we can take each shape and extrude it
+				const geometry = new THREE.ExtrudeGeometry(shape, {
+					depth: 20,
+					bevelEnabled: false
+				});
+
+				// Create a mesh and add it to the group
+				const mesh = new THREE.Mesh(geometry, this.material);
+
+				this.svgGroup.add(mesh);
+			});
+		});
+	}
+
+	init() {
+		this.setup();
+		this.getPaths();
+	}
+
+	foo() {
+		// Meshes we got are all relative to themselves
+		// meaning they have position set to (0, 0, 0)
+		// which makes centering them in the group easy
+
+		// Get group's size
+		const box = new THREE.Box3().setFromObject(this.svgGroup);
+		const size = new THREE.Vector3();
+		box.getSize(size);
+
+		const yOffset = size.y / -2;
+		const xOffset = size.x / -2;
+
+		// Offset all of group's elements, to center them
+		this.svgGroup.children.forEach((item) => {
+			item.position.x = xOffset;
+			item.position.y = yOffset;
+		});
+
+		// Finally we add svg group to the scene
+		scene.add(svgGroup);
+	}
+}
