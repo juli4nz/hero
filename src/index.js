8ac3abcdf6a3d15a66dfe2fd6bf3d9be75b719e4
juli4nz <1939392+juli4nz@users.noreply.github.com>
2020-01-26 15:00:15 +0000

    Add src/print.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './styles.css';
+import printMe from './print.js';
 
 function component() {
 	const element = document.createElement('div');
+	const btn = document.createElement('button');
 
 	element.innerHTML = 'Hello Hero';
 	element.classList.add('hello');
+
+	btn.innerHTML = 'Click me and check the console';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
 
 	return element;
 }
