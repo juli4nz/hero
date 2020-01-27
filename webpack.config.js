8168b6f9430f94d9bf2e0009558d065a772d8548
juli4nz <1939392+juli4nz@users.noreply.github.com>
2020-01-27 14:51:22 +0000

    Configure assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isDev = true;
 
 module.exports = {
-	mode: isDev ? 'development' : 'production',
-	entry: {
-		app: './src/index.js'
-	},
-	output: {
-		filename: '[name].[hash].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	resolve: {
-		extensions: ['.js', '.scss']
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist'
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: './src/index.html'
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].[hash].css',
-			chunkFilename: '[id].[hash].css'
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-			},
-			{
-				test: /\.(js)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			}
-		]
-	}
+  mode: isDev ? "development" : "production",
+  entry: {
+    app: "./src/index.js"
+  },
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".js", ".scss"]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
 };
